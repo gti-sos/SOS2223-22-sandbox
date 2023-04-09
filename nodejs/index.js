00ac8b5472c57279b02c96973540ebae5443e8b9
Carlos Gata <carlosgm46@gmail.com>
2023-04-09 14:01:00 +0200

    probando...

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 const metodosCGM = require('../backend/routeCGM');// GET,PUT,DELETE,POST Carlos Gata Masero
-const metodosACB = require("../backend/routeACB");// GET,PUT,DELETE,POST Antonio Carranza Barroso
+const metodosACB = require("../backend/routeACB");// GET,PUT,DELETE,POST Antonio Carranza Barroso 
 var app = express();
 var port = process.env.PORT || 12345;
 const BASE_API_URL = "/api/v1";
